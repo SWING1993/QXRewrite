15b92eb91d249c76fb871436b7cd3d638a8c9aba
SWING <swing1993@qq.com>
2024-07-09 16:51:32 +0800

    update

---
+++ AdBuster.js
@@ -0,0 +1,29 @@
+/*
+AdBlocker 解锁Premium
+
+[rewrite_local]
+https://api.adblockpro.app/verify url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Github.js
+
+[mitm]
+hostname = api.adblockpro.app
+*/
+
+const obj = {
+    p: 1,
+    a: "",
+    i: 1,
+    x: 1,
+    b: 274,
+    r: "",
+    c: "6BF1B469",
+    s: 1,
+    l: 1,
+    t: 1,
+    e: 1,
+    m: 1,
+    f: 1,
+    v: true
+};
+$done({
+    body: JSON.stringify(obj)
+});
